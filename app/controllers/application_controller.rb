518f90e0b59f3f59887ab36eed5e9a006eafec8d
joannayhs <joannayuhas@gmail.com>
2020-12-17 17:02:11 -0500

    added helpers to app controller for verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+    def current_user 
+        User.find(params[:session_id])
+    end 
+
+    def logged_in? 
+        !!current_user
+    end 
 end
