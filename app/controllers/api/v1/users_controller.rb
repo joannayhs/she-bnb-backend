58252ac51b11642558c4bcde8aeb51308c7dd144
joannayhs <joannayuhas@gmail.com>
2020-12-17 18:41:40 -0500

    added config changes to permit cookies

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,5 +4,5 @@ class Api::V1::UsersController < ApplicationController
         @user = User.find(params[:id])
         render json: UserSerializer.new(@user).serializable_hash.to_json, status: 200
     end 
-    
+
 end
