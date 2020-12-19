4b440766379a164a391768b8ef24af46b0060542
joannayhs <joannayuhas@gmail.com>
2020-12-18 22:17:26 -0500

    fixed user params so user can sign up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,11 +7,12 @@ class Api::V1::UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
+        
         if @user.save 
             render json: UserSerializer.new(@user).serializable_hash.to_json, status: 200
             session[:user_id] = @user.id 
         else 
-            alert("An error occured")
+            render json: alert(@user.errors.message)
         end
     end 
 
