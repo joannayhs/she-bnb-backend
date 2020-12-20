3ed711b3792bd549f43167cf9ed549e5c6da8973
joannayhs <joannayuhas@gmail.com>
2020-12-19 21:18:56 -0500

    added reservations to user serializer

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::ReservationsController < ApplicationController
+
+    def show 
+        @reservation = Reservation.find(params[:id])
+        render json: ReservationSerializer.new(@reservation).serializable_hash.to_json, status:200
+    end 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,14 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :email, :first_name, :last_name
+  attribute :reservations do |user|
+    user.reservations.each do |reservation|
+      {
+        start_date: reservation.start_date,
+        end_date: reservation.end_date
+      }
+    end
+  end 
   has_many :reviews
   has_many :listings
-  has_many :reservations
 end
