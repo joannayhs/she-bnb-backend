03adffc622e8004ba1ef894f85212d4c45d0f4a1
joannayhs <joannayuhas@gmail.com>
2020-12-23 13:17:35 -0500

    added amenities serializer, route, and index

--- app/controllers/api/v1/amenities_controller.rb
+++ app/controllers/api/v1/amenities_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::AmenitiesController < ApplicationController
+
+    def index 
+        @amenities = Amenities.all
+        render json: AmenitiesSerializer.new(@amenities).seralizable_hash.to_json
+    end 
 end
---
+++ app/serializers/amenities_serializer.rb
@@ -0,0 +1,4 @@
+class AmenitiesSerializer
+  include JSONAPI::Serializer
+  attributes :name
+end
