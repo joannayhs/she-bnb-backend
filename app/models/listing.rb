96b4aa00f4731b2dd33e897bcc673c360e89e11a
joannayhs <joannayuhas@gmail.com>
2020-12-24 21:52:52 -0500

    added validations to listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,9 @@
 class Listing < ApplicationRecord
     belongs_to :user
+
+    validates :title, presence: true, uniqueness: true
+    validates :description, :type_of, :max_guests, :num_of_beds, :price, presence: true
+
     has_many :reservations
     has_one :property, dependent: :destroy
     has_and_belongs_to_many :amenities
