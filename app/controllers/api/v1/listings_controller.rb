39519d8cf377737b2eb085f3760eaf3659633048
joannayhs <joannayuhas@gmail.com>
2021-01-03 15:31:12 -0500

    added update method to listing controller

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -18,6 +18,15 @@ class Api::V1::ListingsController < ApplicationController
             render json: {error: "Could not save listing"}
         end 
     end 
+
+    def update
+        @listing = Listing.find(params[:id])
+        if @listing.update(listing_params)
+            render json: ListingSerializer.new(@listing).serializable_hash.to_json, status: 200
+        else 
+            render json: { error: "Could not update listing"}
+        end
+    end 
     
 
     private 
