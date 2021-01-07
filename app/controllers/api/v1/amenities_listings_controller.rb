d500e42dc3fe2009d4d68391eaf810f0d7744ee9
joannayhs <joannayuhas@gmail.com>
2021-01-07 15:36:21 -0500

    fixed issue where amnenity was destroyed instead of delete from listing

--- app/controllers/api/v1/amenities_listings_controller.rb
+++ app/controllers/api/v1/amenities_listings_controller.rb
@@ -3,8 +3,11 @@ class Api::V1::AmenitiesListingsController < ApplicationController
 
     def create 
         @listing = Listing.find(params[:listing_id])
-        @listing.amenities << Amenity.find_by(name: params[:name])
-        render json: ListingSerializer.new(@listing).serializable_hash.to_json, status: 200
+        if @listing.amenities << Amenity.find_by(name: params[:name])
+            render json: ListingSerializer.new(@listing).serializable_hash.to_json, status: 200
+        else
+            render json: {error: "Unable to add amenity to listing"}
+        end
     end 
 
     def destroy
